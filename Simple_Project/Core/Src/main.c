11e3e0468f82813147f55e0a91f8f1abd7693abe
GoldenTalon <mrcoultis@gmail.com>
2020-09-19 19:42:32 -0500

    Removed dead code.

--- Simple_Project/Core/Src/main.c
+++ Simple_Project/Core/Src/main.c
@@ -58,8 +58,6 @@ PCD_HandleTypeDef hpcd_USB_OTG_FS;
 uint8_t buf[30];
 int8_t number1;
 int8_t number2;
-static const uint8_t TMP102_ADDR = 0x48 << 1; // Use 8-bit address
-static const uint8_t REG_TEMP = 0x00;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -88,12 +86,9 @@ static void MX_USB_OTG_FS_PCD_Init(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-  HAL_StatusTypeDef ret;
   uint8_t buf[12];
-  int16_t val;
-  float temp_c;
+  int32_t val;
   int8_t num1;
-  int8_t num2;
   /* USER CODE END 1 */
 
 
@@ -123,8 +118,7 @@ int main(void)
     MX_USART3_UART_Init();
     MX_USB_OTG_FS_PCD_Init();
   /* USER CODE BEGIN 2 */
-    num1 = 5;
-    num2 =10;
+    num1 = 50;
     val = 0;
   /* USER CODE END 2 */
 
@@ -132,40 +126,11 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	val = __QADD16(val,num1);
 
-    // Tell TMP102 that we want to read from the temperature register
-    buf[0] = REG_TEMP;
-    ret = HAL_I2C_Master_Transmit(&hi2c2, TMP102_ADDR, buf, 1, HAL_MAX_DELAY);
-    /*
-    if ( ret != HAL_OK ) {
-      strcpy((char*)buf, "Error Tx\r\n");
-    } else {
-
-      // Read 2 bytes from the temperature register
-      ret = HAL_I2C_Master_Receive(&hi2c2, TMP102_ADDR, buf, 2, HAL_MAX_DELAY);
-      if ( ret != HAL_OK ) {
-        strcpy((char*)buf, "Error Rx\r\n");
-      } else {
-      */
-        //Combine the bytes
-        //val = ((int16_t)buf[0] << 4) | (buf[1] >> 4);
-    	val = __QADD16(val,num2);
-        // Convert to 2's complement, since temperature can be negative
-        //if ( val > 0x7FF ) {
-        //  val |= 0xF000;
-        //}
-
-        // Convert to float temperature value (Celsius)
-        temp_c = val;
-        //temp_c = val * 0.0625;
-
-        // Convert temperature to decimal format
-        temp_c *= 100;
-        sprintf((char*)buf,
-              "QADD Result: %u\r\n",
-              ((unsigned int)val));
-     // }
-    //}
+    sprintf((char*)buf,
+    "QADD Result: %u\r\n",
+    ((unsigned int)val));
 
     // Send out buffer (temperature or error message)
     HAL_UART_Transmit(&huart1, buf, strlen((char*)buf), HAL_MAX_DELAY);
